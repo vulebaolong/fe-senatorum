b440450952b6b1d0236c9200b8a5575874fae608
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-09-15 18:19:11 +0700

    1

--- src/api/actions/type.action.ts
+++ src/api/actions/type.action.ts
@@ -9,7 +9,7 @@ export async function getListTypeArticleAction(): Promise<TResAction<TType[] | n
     try {
         const result = await api.get<TRes<TType[]>>(`${ENDPOINT.TYPE.TYPE}?pageSize=9999`);
         const { data } = result;
-        console.log({ getListTypeArticleAction: data });
+        // console.log({ getListTypeArticleAction: data });
         return { status: "success", message: result.message, data: data };
     } catch (error: any) {
         return { status: "error", message: error?.message, data: null };
